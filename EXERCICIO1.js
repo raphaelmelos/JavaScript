2a5e2428a44e05d2abf4be145d9123a81c0d24da
Raphael <raphamelo41@gmail.com>
2025-09-02 21:28:58 -0300

    add if, else

--- EXERCICIO1.js
+++ EXERCICIO1.js
@@ -4,3 +4,9 @@ const anoNascimento = 1999;
 const idade = anoAtual - anoNascimento;
 
 console.log(idade);
+
+if (idade < 18) {
+    console.log("Você é menor de idade")
+} else (idade > 18) {
+    console.log("Você já é maior de idade")
+}
