c6d78a1a9665ce0af3fbca2db941abeb525ee510
Raphael <raphamelo41@gmail.com>
2025-09-03 21:07:03 -0300

    input

--- EXERCICIO1.js
+++ EXERCICIO1.js
@@ -10,3 +10,16 @@ if (idade < 18) {
 } else (idade > 18) {
     console.log("Você já é maior de idade")
 }
+
+let nome = prompt("Digite seu nome:");
+console.log("O nome digitado é: " + nome);
+
+// Seleciona o input pelo ID
+let meuInput = document.getElementById("meuInput");
+
+// Adiciona um "ouvinte" para um evento (ex: keyup, que dispara a cada tecla pressionada)
+meuInput.addEventListener("keyup", function() {
+  // Obtém o valor atual do input
+  let valorDigitado = meuInput.value;
+  console.log("Valor atual:", valorDigitado);
+});
